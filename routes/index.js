cf9b591928403d8966a44dfed9ebfbee95c7627c
SamSunny95 <sampsunny1995@gmail.com>
2016-09-02 18:01:49 -0400

    added Contact Me SMTP and background to skills page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req,res,next){
+  var mailOpts, transporter;
+  transporter = nodemailer.createTransport();
+ 
+  mailOpts = {
+      from: req.body.name + ' &lt;' + req.body.email, //grab form data from the request body object
+      to: 'sampsunny1995@gmail.com',
+      subject: req.body.subject,
+      text: req.body.message
+  };
+  transporter.sendMail(mailOpts, function (error, response) {
+      //Email not sent
+      if (error) {
+          console.log(error);
+      }
+      //Yay!! Email sent
+      else {
+        console.log("success")
+          res.render('index', { title: 'Express' });
+      }
+  });
+  transporter.close();
 });
 
 module.exports = router;

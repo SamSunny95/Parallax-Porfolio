e6c1ae65149268149575d10c9e7d42cfa2081032
Sam Sunny <sampsunny1995@gmail.com>
2018-09-12 23:52:01 -0400

    updated copyright and loader timeout

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -157,7 +157,7 @@ window.addEventListener("load", function(){
   var loader = document.getElementById("loader");
   window.setTimeout(function(){
   document.body.removeChild(loader)
-  },500);
+  },300);
   
 });
 
